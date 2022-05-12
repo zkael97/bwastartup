d3e399a782259c091b78c48f5b511f03079c5777
firdausdicko97 <firdausdicko@gmail.com>
2022-05-12 17:45:34 +0700

    third

--- handler/user.go
+++ handler/user.go
@@ -1,1 +1,19 @@
-package user
+package handler
+
+import (
+	"bwastartup/user"
+
+	"github.com/gin-gonic/gin"
+)
+
+type userHandler struct {
+	userService user.Service
+}
+
+func NewUserHandler(userSevice user.Service) *userhandler {
+	return &userHandler(userSevice)
+}
+
+func (h *userHandler) RegisterUser(c *gin.Context) {
+
+}
