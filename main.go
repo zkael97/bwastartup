550a3ba97d4bd06af18227ff7890f4ae50ea6d26
firdausdicko97 <firdausdicko@gmail.com>
2022-05-12 16:30:32 +0700

    Update 1

---
+++ main.go
@@ -0,0 +1,25 @@
+package main
+
+import (
+	"bwastartup/user"
+	"log"
+
+	"gorm.io/driver/mysql"
+	"gorm.io/gorm"
+)
+
+func main() {
+	dsn := "root:@tcp(127.0.0.1:3306)/bwastartup?charset=utf8mb4&parseTime=True&loc=Local"
+	db, err := gorm.Open(mysql.Open(dsn), &gorm.Config{})
+
+	if err != nil {
+		log.Fatal(err.Error())
+	}
+
+	userRepository := user.NewRepository(db)
+	user := user.User{
+		Name: "Test",
+	}
+	userRepository.Save(user)
+
+}
