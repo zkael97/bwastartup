5b40e5ec9ce6982c903b16abf594edcb0277f29c
firdausdicko97.com <firdausdicko@gmail.com>
2022-07-06 00:07:18 +0700

    Update repository Change Avatar

--- user/repository.go
+++ user/repository.go
@@ -6,6 +6,8 @@ import "gorm.io/gorm"
 type Repository interface {
 	Save(user User) (User, error)
 	FindByEmail(email string) (User, error)
+	FindByID(ID string) (User, error)
+	Update(user User) (User, error)
 }
 
 type repository struct {
@@ -28,6 +30,24 @@ func (r *repository) Save(user User) (User, error) {
 func (r *repository) FindByEmail(email string) (User, error) {
 	var user User
 	err := r.db.Where("email = ?", email).Find(&user).Error
+	if err != nil {
+		return user, err
+	}
+	return user, nil
+}
+
+func (r *repository) FindByID(ID string) (User, error) {
+	var user User
+
+	err := r.db.Where("id = ?", ID).Find(&user).Error
+	if err != nil {
+		return user, err
+	}
+	return user, nil
+}
+
+func (r *repository) Update(user User) (User, error) {
+	err := r.db.Save(&user).Error //melakukan perubahan data dari struct atau data yang sudah ada
 	if err != nil {
 		return user, err
 	}
